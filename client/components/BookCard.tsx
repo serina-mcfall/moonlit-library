13ff1520668417e6731b5137a6e1d222f3b799b7
Serina Mcfall <serina.mcfall@gmail.com>
2026-06-10 11:23:53 +1200

    Add ManaBar to book cards

--- client/components/BookCard.tsx
+++ client/components/BookCard.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router'
 import type { Book } from '../../models/books'
+import ManaBar from './ManaBar'
 
 interface Props {
   book: Book
@@ -8,8 +9,14 @@ interface Props {
 function BookCard({ book }: Props) {
   return (
     <Link to={`/books/${book.id}`}>
+      <img
+        src={book.cover_image ?? ''}
+        alt={book.title}
+        style={{ width: '120px', height: '180px', objectFit: 'cover' }}
+      />
       <h3>{book.title}</h3>
       <p>by {book.author}</p>
+      <ManaBar status={book.read_status} />
     </Link>
   )
 }
